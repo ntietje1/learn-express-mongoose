ba7c5c05e95e8df6d29cfa3361e580e0df32339e
Nicholas Tietje <tietje.n@northeastern.edu>
2024-03-13 16:52:35 -0400

    inclass activity

--- models/genre.js
+++ models/genre.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var GenreSchema = new Schema(
-  {}
-);
-
+var GenreSchema = new Schema({
+  name: { type: String, required: true },
+  url: { type: String },
+});
 
 //Export model
-module.exports = mongoose.model('Genre', GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require("../models/bookinstance");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = function (res) {
+  BookInstance.find(
+    { status: "available" },
+    "title status",
+    function (err, bookInstances) {
+      if (err) {
+        return next(err);
+      }
+      res.send(bookInstances);
+    }
+  );
+};
